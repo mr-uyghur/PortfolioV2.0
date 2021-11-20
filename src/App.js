1c1bc4b523fd1a8eeae755d8c4fc8c517e2fe40e
alimran halmamat <mr.uygur1996@gmail.com>
2021-11-20 12:33:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state={
+      str:"Ali"
+    }
+  }
+
+  render() {
+    return ( <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+      {this.state.str}
+      </p>
+      <button
+      >
+       click me
+      </button>
+    </header>
+  </div>)
+  }
 }
+
+
 
 export default App;
