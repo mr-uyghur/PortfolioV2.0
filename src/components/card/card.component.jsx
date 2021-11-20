5a72cd4a1959746a4f278b17e33265e351c7a2e1
alimran halmamat <mr.uygur1996@gmail.com>
2021-11-20 15:09:12 -0800

    updated front add with img tag, and the img src comes from API

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,9 @@ import './card.styles.css'
 export const Card = (props) =>{
     return(
         <div className = "card-container">
+            <img alt = "moster" src = {`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
           <h1>  {props.monster.name} </h1>
+          <h3>{props.monster.email}</h3>
             </div>
     )
 }
